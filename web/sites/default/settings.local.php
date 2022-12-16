92a36285a4c30b6787d1b26147b96e411e5a7cfd
Omar El Maguiri <omarelmaguiri@gmail.com>
2022-12-16 23:01:45 +0100

    Custom module tawjeeh_content creation

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -153,3 +153,4 @@ $settings['skip_permissions_hardening'] = TRUE;
  * the language or field module.
  */
 # $settings['config_exclude_modules'] = ['devel', 'stage_file_proxy'];
+$settings['config_ignore_deactivate'] = TRUE;
